8a1c66382656738cb4a33cfe67d885c60378a9b9
BQLogics <itdepartmentktpm@gmail.com>
2024-08-21 16:36:38 +0530

    test day2

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def index():
-    return "<h1>Hello world</h1>"
+    return "<h1>this is next day test1</h1>"
 
-
-if __name__ == '__main__' :
-    app.run(host='0.0.0.0', port=4444 , debug=True )
+if __name__ == '__main__':
+    app.run(debug=True, port=1234)
