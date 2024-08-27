98d60caf916fd78aaebf958ca9046788abd1f84a
BQLogics <itdepartmentktpm@gmail.com>
2024-08-27 16:33:34 +0530

    test 7

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ app = Flask (__name__,template_folder="templates")
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template("index.html")
 
 @app.route("/greeting/<name>")
 def greeting(name):
     return f"Hell Good evening {name}"
 
-@app.route ("/summation/<int:num1>/<int:num2>")
-def sum(num1,num2):
-    return f"{num1} + {num2} = {num1+num2}"
+# @app.route ("/summation/<int:num1>/<int:num2>")
+# def sum(num1,num2):
+#     return f"{num1} + {num2} = {num1+num2}"
 
-@app.route ('/url_params')
-def params():
-    return str(request.args)
+# @app.route ('/url_params')
+# def params():
+#     return str(request.args)
 
-@app.route("/home")
-def home():
-    return f"this is another test6"
+# @app.route("/home")
+# def home():
+#     return f"this is another test6"
 
 if __name__ == "__main__":
     app.run (debug=True)
